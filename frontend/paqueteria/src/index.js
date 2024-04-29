d6857a4dde4235528fdc7027975efda43b20ba9e
charlyus <carloslopez201831102@cunoc.edu.gt>
2024-04-28 23:41:21 -0600

    localizaion paquete

--- frontend/paqueteria/src/index.js
+++ frontend/paqueteria/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AdminLogin from './AdminLogin'; // Importa el componente AdminLogin
-import Operadores from './Operadores'
-import Recepcionistas from './Recepcionistas'
+import Operadores from './Operadores';
+import Recepcionistas from './Recepcionistas';
+import SeguimientoPaquete from './SeguimientoPaquete';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Recepcionistas /> {/* Incluye el componente AdminLogin */}
+    <SeguimientoPaquete /> {/* Incluye el componente AdminLogin */}
   </React.StrictMode>,
   document.getElementById('root')
 );
